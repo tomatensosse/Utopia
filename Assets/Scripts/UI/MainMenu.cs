c8cc7472dd86b72eff3e9a69853a97d7025992b3
Darkgoatie <haliterenozkir@gmail.com>
2024-09-23 22:42:22 +0300

    UI Upgrade

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 using Mirror;
 using TMPro;
 using UnityEngine;
@@ -10,6 +12,8 @@ public class MainMenu : MonoBehaviour
 
     [Header("UI")]
     private string currentSelection;
+
+    public GameObject[] allSelectionUI;
 
     public GameObject selectionUI;
 
@@ -45,6 +49,8 @@ public class MainMenu : MonoBehaviour
     private SaveSystem saveSystem = new SaveSystem();
     
     private CustomNetworkManager networkManager;
+    public ErrorPopup errorPopup; 
+    public TMP_Dropdown characterClassSelectDropdown; // Assign in the Inspector
 
     private void Awake()
     {
@@ -73,54 +79,51 @@ public class MainMenu : MonoBehaviour
         switch (selection)
         {
             case "SelectionUI":
+                DeactivateAllUI();
                 selectionUI.SetActive(true);
-                playerCreationUI.SetActive(false);
-                worldCreationUI.SetActive(false);
-                joinSelectionUI.SetActive(false);
-                titleScreenUI.SetActive(false);
-                settingsUI.SetActive(false);
                 break;
             case "PlayerCreationUI":
-                selectionUI.SetActive(false);
+                DeactivateAllUI();
                 playerCreationUI.SetActive(true);
-                worldCreationUI.SetActive(false);
-                joinSelectionUI.SetActive(false);
-                titleScreenUI.SetActive(false);
-                settingsUI.SetActive(false);
                 break;
             case "WorldCreationUI":
-                selectionUI.SetActive(false);
-                playerCreationUI.SetActive(false);
+                DeactivateAllUI();
                 worldCreationUI.SetActive(true);
-                joinSelectionUI.SetActive(false);
-                titleScreenUI.SetActive(false);
-                settingsUI.SetActive(false);
                 break;
             case "JoinSelectionUI":
-                selectionUI.SetActive(false);
-                playerCreationUI.SetActive(false);
-                worldCreationUI.SetActive(false);
+                DeactivateAllUI();
                 joinSelectionUI.SetActive(true);
-                titleScreenUI.SetActive(false);
-                settingsUI.SetActive(false);
                 break;
             case "TitleScreenUI":
-                selectionUI.SetActive(false);
-                playerCreationUI.SetActive(false);
-                worldCreationUI.SetActive(false);
-                joinSelectionUI.SetActive(false);
+                DeactivateAllUI();
                 titleScreenUI.SetActive(true);
-                settingsUI.SetActive(false);
                 break;
             case "SettingsUI":
-                selectionUI.SetActive(false);
-                playerCreationUI.SetActive(false);
-                worldCreationUI.SetActive(false);
-                joinSelectionUI.SetActive(false);
-                titleScreenUI.SetActive(false);
+                DeactivateAllUI();
                 settingsUI.SetActive(true);
                 break;
         }
+    }
+    
+    public void DeactivateAllUI()
+    {
+        // LIST ALL UI HERE!!!!
+        allSelectionUI = new GameObject[] {
+            selectionUI,
+            playerCreationUI,
+            worldCreationUI,
+            joinSelectionUI,
+            titleScreenUI,
+            settingsUI
+        };
+
+        foreach (GameObject ui in allSelectionUI)
+        {
+            if (ui != null) {
+                ui.SetActive(false);
+            }
+        };
+
     }
 
     public void LoadSelectionMenuContent()
@@ -177,13 +180,43 @@ public class MainMenu : MonoBehaviour
 
     public void CreatePlayer()
     {
+        string inputName = playerNameInput.text;
+        string fullInputClass = characterClassSelectDropdown.options[characterClassSelectDropdown.value].text;
+        string inputClass = fullInputClass.Split(':')[0];
+        Debug.Log(inputClass);
+
+        string validNameRegex = "^[a-zA-Z0-9._-]{1,20}$";
+
+        if (validateRegex(inputName,validNameRegex) != true) { // Validate Name
+            errorPopup.ShowError("Name should be 1-20 letters, include only a-z, A-Z, and . _ -", 4);
+            return;
+        }
+
+        string[] playerNames = saveSystem.LoadAllPlayers().Select(ply => ply.playerSaveName).ToArray();
+        
+        if (playerNames.Contains(inputName) == true) {
+            errorPopup.ShowError("The name \"" + inputName + "\" is already taken!", 4);
+            return;
+        }
+
+        if (inputClass == "Choose Class") {
+            errorPopup.ShowError("Please choose a class!", 4);
+            return;
+        }   
+
         PlayerData playerSave = new PlayerData();
-        playerSave.playerSaveName = playerNameInput.text;
-        playerSave.playerClass = playerClassInput.text;
-        saveSystem.SavePlayer(playerSave, playerSave.playerSaveName);
+        playerSave.playerSaveName = inputName;
+        playerSave.playerClass = inputClass;
+        saveSystem.SavePlayer(playerSave, inputName);
         ChangeTo("SelectionUI");
         LoadSelectionMenuContent();
     }
+
+    public bool validateRegex(string nameToCheck, string RegexToCheck) 
+    {
+        return Regex.IsMatch(nameToCheck, RegexToCheck);
+    }
+
 
     public void CreateWorld()
     {
