92e07104ce08092c3adfe654a1c0add9773762b1
Whem <106603573+WhemDev@users.noreply.github.com>
2024-09-24 16:05:58 +0300

    enemyGroups template progress

---
+++ Assets/Scripts/NPCs/EnemyGroups.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCGroup : MonoBehaviour
+{
+    [Header("Group Settings")]
+    public int maxMembers = 8; // Maksimum grup üyesi 
+    public int healerCount = 2; // Healer 
+    public int tankCount = 2;   // Tank 
+    public int warriorCount = 3; // Warrior 
+    public GameObject EnemyPrefab; // galiba gang'in hangi tür enemyden oluşacağını belirliyo ama valla bilmiyom
+    private List<NPC> groupMembers = new List<NPC>(); 
+    private NPC leader; // inş private olması doğrudur
+}
